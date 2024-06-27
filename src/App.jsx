4492c6114c7929dd0e4da1e5772f1091accadb0b
Albina Khomenko <klementina.ua@gmail.com>
2024-06-27 22:43:09 +0200

    render cards on page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
-import { useState } from 'react'
-import { Container, Row, Col } from 'react-grid-system';
-import './App.css'
+import { useState } from "react";
+import { Container, Row, Col } from "react-grid-system";
+import data from "./data/allCards";
+import "./App.css";
 
 function App() {
+	const [cards, setCards] = useState(data.cards);
 
-
-  return (
-    <Container>
-      <Row>
-        <Col sm={6}></Col>
-      </Row>
-    </Container>
-  )
+	return (
+		<Container>
+			<Row>
+				{cards.map((card) => (
+					<Col sm={2} key={card.id}>
+						<img
+							src={card.back_side}
+							alt="back_side"
+							style={{
+								width: "100%",
+								borderRadius: "8px",
+								marginBottom: "16px",
+								cursor: "pointer",
+							}}
+						/>
+					</Col>
+				))}
+			</Row>
+		</Container>
+	);
 }
 
-export default App
+export default App;
