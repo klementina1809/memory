3a9a1e24bf87dfdc5b3ae34231248bf4176526a1
Albina Khomenko <klementina.ua@gmail.com>
2024-06-27 23:41:14 +0200

    flipping card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,29 @@
-import React from 'react'
+import { useState } from "react";
 
-function Card({data}) {
-  return (
-    <div className='card'>
-      	<img
-							src={data.back_side}
-							alt="back_side"
-							style={{
-								width: "100%",
-								borderRadius: "8px",
-								marginBottom: "16px",
-								cursor: "pointer",
-							}}
-						/>
-    </div>
-  )
+function Card({ data }) {
+	const [sideOfCard, setSideOfCard] = useState("back");
+
+	const handleFlip = (data) => {
+		console.log("data", data);
+		sideOfCard === "back" ? setSideOfCard("front") : setSideOfCard("back");
+	};
+
+	return (
+		<div className="card">
+			<img
+				onClick={(e) => handleFlip(e.target)}
+				side={sideOfCard}
+				src={sideOfCard === "back" ? data.back_side : data.front_side}
+				alt="back_side"
+				style={{
+					width: "100%",
+					borderRadius: "8px",
+					marginBottom: "16px",
+					cursor: "pointer",
+				}}
+			/>
+		</div>
+	);
 }
 
-export default Card
+export default Card;
