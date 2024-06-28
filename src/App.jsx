a547903762b26e76636c52cecfbf28ca468d54f9
Albina Khomenko <klementina.ua@gmail.com>
2024-06-28 18:36:44 +0200

    added count of flipped cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-grid-system";
 import Card from "./components/Card";
 import data from "./data/allCards";
@@ -6,13 +6,20 @@ import "./App.css";
 
 function App() {
 	const [cards, setCards] = useState(data.cards);
+	const [flippedCount, setFlippedCount] = useState(0);
+
+	useEffect(() => {
+		const count = cards.filter((card) => card.isFlipped).length;
+		setFlippedCount(count);
+		console.log("count", count)
+	}, [cards]);
 
 	return (
 		<Container>
 			<Row>
 				{cards.map((card) => (
 					<Col sm={2} key={card.id}>
-						<Card data={card} />
+						<Card data={card} setCards={setCards} cards={cards} />
 					</Col>
 				))}
 			</Row>
