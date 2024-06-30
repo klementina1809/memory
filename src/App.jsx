4576b82c9396f05100d53d0960a12f9d0920259f
Albina Khomenko <klementina.ua@gmail.com>
2024-07-01 00:09:35 +0200

    added shuffleArray

--- src/App.jsx
+++ src/App.jsx
@@ -5,42 +5,55 @@ import data from "./data/allCards";
 import "./App.css";
 
 function App() {
-	const [cards, setCards] = useState(data.cards);
+	const [cards, setCards] = useState([]);
 	const [flippedCount, setFlippedCount] = useState(0);
 
-	const handleControl = (count, filteredCards) => {
-		if (count === 2) {
-			const result = filteredCards[0].name === filteredCards[1].name;
-			console.log("result", result);
-			setTimeout(() => {
-				setCards((prevCards) =>
-					prevCards.map((card) =>
-						card.id === filteredCards[0].id ||
-						card.id === filteredCards[1].id
-							? { ...card, isFlipped: result }
-							: card
-					)
-				);
-			}, 1000);
-		}
-	};
+	useEffect(() => {
+		setCards(shuffleArray(data.cards));
+	}, []);
 
 	useEffect(() => {
 		const filteredCards = cards.filter((card) => card.isFlipped);
 		const count = filteredCards.length;
-		handleControl(count, filteredCards);
-
+		if (count % 2 === 0 && count !== 0) {
+			handleControl(count, filteredCards);
+		}
 		setFlippedCount(count);
 		console.log("count", count);
 	}, [cards]);
 
+	const handleControl = (count, filteredCards) => {
+		const result =
+			filteredCards[filteredCards.length - 1].name ===
+			filteredCards[filteredCards.length - 2].name;
+		console.log("result", result);
+		setTimeout(() => {
+			setCards((prevCards) =>
+				prevCards.map((card) =>
+					card.id === filteredCards[filteredCards.length - 1].id ||
+					card.id === filteredCards[filteredCards.length - 2].id
+						? { ...card, isFlipped: result }
+						: card
+				)
+			);
+		}, 1000);
+	};
+
+	const shuffleArray = (array) => {
+		for (let i = array.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			[array[i], array[j]] = [array[j], array[i]];
+		}
+		return array;
+	};
+
 	const handleFlip = (id) => {
-    setCards((prevCards) =>
-      prevCards.map((card) =>
-        card.id === id ? { ...card, isFlipped: !card.isFlipped } : card
-      )
-    );
-  };
+		setCards((prevCards) =>
+			prevCards.map((card) =>
+				card.id === id ? { ...card, isFlipped: !card.isFlipped } : card
+			)
+		);
+	};
 
 	return (
 		<Container>
